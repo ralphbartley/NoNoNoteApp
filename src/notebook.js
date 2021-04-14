1cea1d3af656021f73d3a464efe98c5d370c67a7
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-04-14 15:25:29 +0100

    TDD abbreviate functionality

--- src/notebook.js
+++ src/notebook.js
@@ -1,15 +1,24 @@
 class Notebook {
-    constructor() {
-        this.array = []
-    }
-    
+	constructor() {
+		this.notes = []
+	}
+	
+	list () {
+		console.log(this.notes)
+		return this.notes
+	}
 
-    list () {
-        console.log(this.array)
-        return this.array
-    }
+	create (fullText) {
+	  return this.notes.push(fullText)
+	}
 
-    create (fullText) {
-       return this.array.push(fullText)
+  abbreviate (fullText) {
+    return fullText.slice(0,20)
+  }
+
+  listAbbreviated () {
+    for(let i = 0; i < this.notes.length; i++) {
+      this.notes[i] = this.abbreviate(this.notes[i]);
     }
+  }
 }
