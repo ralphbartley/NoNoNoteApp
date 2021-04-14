c742e653a003e80f4772a92e829ea6ba8392cac6
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-04-14 19:16:03 +0100

    Implemented delete note and edit note!!!

--- src/interface.js
+++ src/interface.js
@@ -11,12 +11,13 @@ function updateNoteList() {
   var newNoteList = "";
   notebook.list().forEach((note, index) => {
     note = notebook.abbreviate(note);
-    var listItem = `<li><a href=#${index}>${note}</a></li>`;
+    var listItem = `<li><a href=#${index}>${note}</a><button type="button" id=${index}> X </button></li>`;
     newNoteList += listItem;
   });
 
   var notesSection = document.getElementById("list_notes");
   notesSection.innerHTML = newNoteList;
+  deleteButtonListner()
 }
 
 var createButton = document.getElementById("save_note");
@@ -34,9 +35,27 @@ function displayNote() {
 function getNote() {
   var index = window.location.hash.split("#")[1];
   document.getElementById("note_text").value = notebook.notes[index];
-  notebook.notes.pop(index);
+  notebook.notes = arrayRemove(notebook.notes, notebook.notes[index]);
   updateNoteList();
   history.replaceState(null, null, document.location.pathname);
 }
 
+function deleteButtonListner() {
+  var notes = document.getElementById("list_notes");
+  var buttons = notes.getElementsByTagName("button");
+  for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", deleteNote);
+  }
+}
 
+function deleteNote(event) {
+  var button_id = event.target.id
+  notebook.notes = arrayRemove(notebook.notes, notebook.notes[button_id]);
+  updateNoteList();
+}
+
+function arrayRemove(arr, value) { 
+  return arr.filter(function(ele){ 
+    return ele != value; 
+  });
+}
